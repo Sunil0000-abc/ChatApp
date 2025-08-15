8e172cdb35354acda4e2a60d70dbd2a469f03db7
Sunil0000-abc <sd3234746@gmail.com>
2025-08-15 13:02:28 +0530

    Link added

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ const server = http.createServer(app);
 const PORT = 5000;
 const io = new Server(server, {
   cors: {
-    origin: "https://chat-app-nu-flax.vercel.app",
+    origin: "https://chat-app-fde1.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
